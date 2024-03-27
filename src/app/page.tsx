20ea3b84fe4dc247b68c9638450ccc9ea1711bd3
Nutchapon Hanouypornlert <nutchaponhanouypornlert@Nutchapons-MacBook-Pro.local>
2024-03-27 16:04:46 +0700

    chor: add montserrat fint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
       </Head>
       <section className='bg-white'>
         <div>
-          hello
+          Whereas recognition of the inherent dignity
         </div>
       </section>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,24 +6,16 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        primary: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        montserrat: ['Montserrat']
       },
       colors: {
         primary: {
-          // Customize it on globals.css :root
-          50: 'rgb(var(--tw-color-primary-50) / <alpha-value>)',
-          100: 'rgb(var(--tw-color-primary-100) / <alpha-value>)',
-          200: 'rgb(var(--tw-color-primary-200) / <alpha-value>)',
-          300: 'rgb(var(--tw-color-primary-300) / <alpha-value>)',
-          400: 'rgb(var(--tw-color-primary-400) / <alpha-value>)',
-          500: 'rgb(var(--tw-color-primary-500) / <alpha-value>)',
-          600: 'rgb(var(--tw-color-primary-600) / <alpha-value>)',
-          700: 'rgb(var(--tw-color-primary-700) / <alpha-value>)',
-          800: 'rgb(var(--tw-color-primary-800) / <alpha-value>)',
-          900: 'rgb(var(--tw-color-primary-900) / <alpha-value>)',
-          950: 'rgb(var(--tw-color-primary-950) / <alpha-value>)',
+          dark: "#000000",
+          navy: "#150050",
+          purple: "#3F0071",
+          pink: "#FB2576"
         },
-        dark: '#222222',
       },
       keyframes: {
         flicker: {
